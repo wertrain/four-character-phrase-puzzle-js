9b5ce8be73bda6a9829d6f20e6f667d25b104c0b
wertrain <wertrain@gmail.com>
2020-11-09 23:08:50 +0900

    WIP

--- js/four-character-phrase.js
+++ js/four-character-phrase.js
@@ -1,10 +1,24 @@
-var FourCharacterPhrasePuzzle = {};
 {
   'use strict';
+  
+  const CHARACTER_SIZE = 64;
 
-  FourCharacterPhrasePuzzle.FourCharacterPhrase = {};
+  FourCharacterPhrasePuzzle.FourCharacterPhrase = class {
+    constructor(fcp) {
+      this.text = fcp;
+    }
+    indexOf(i) {
+      return this.text[i]
+    }
+    get length() {
+      return this.text.length;
+    }
+  }
 
-  FourCharacterPhrasePuzzle.FourCharacterPhrase.initialize = function() {
-
+  FourCharacterPhrasePuzzle.draw = function(context, fcp, x, y) {
+    context.font = CHARACTER_SIZE + 'px serif';
+    for (let i = 0; i < fcp.length; ++i) {
+      context.fillText(fcp.indexOf(i), x, y + (CHARACTER_SIZE * i));
+    }
   };
 }
