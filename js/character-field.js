5b20686ec9ba509a361fd7f6b68e513ca0e098dd
wertrain <wertrain@gmail.com>
2020-11-18 01:12:18 +0900

    WIP

--- js/character-field.js
+++ js/character-field.js
@@ -1,15 +1,22 @@
 {
   'use strict';
   
+  /** 四字熟語の一文字あたりの描画サイズ */
+  const FIELD_SIZE = 64;
+
   /**
    * 文字表示フィールド
    */
   FourCharacterPhrasePuzzle.CharacterField = class {
-    constructor(fcp) {
-      this.fcp = fcp;
+    constructor(col) {
+      this.col = col;
     }
-    indexOf(i) {
-      return this.text[i]
+    draw(context, x, y) {
+      for (let c = 0; c < this.col; ++c) {
+        for (let i = 0; i < 4; ++i) {
+          context.strokeRect(x + c * FIELD_SIZE, y + (FIELD_SIZE * i), FIELD_SIZE, FIELD_SIZE);
+        }
+      }
     }
     get length() {
       return this.text.length;
