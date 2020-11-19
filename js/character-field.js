0a7b58193e13d01d2211b1187b4a1d80fc08d701
wertrain <wertrain@gmail.com>
2020-11-20 01:26:34 +0900

    WIP

--- js/character-field.js
+++ js/character-field.js
@@ -2,7 +2,10 @@
   'use strict';
   
   /** 四字熟語の一文字あたりの描画サイズ */
-  const FIELD_SIZE = 64;
+  const FIELD_SIZE = 100;
+  const FIELD_SIZE_HALF = FIELD_SIZE / 2;
+  const CHARACTER_SIZE = 80;
+  const CHARACTER_SIZE_HALF = CHARACTER_SIZE / 2;
 
   /**
    * 文字表示フィールド
@@ -12,9 +15,16 @@
       this.col = col;
     }
     draw(context, x, y) {
+      context.font = CHARACTER_SIZE + 'px serif';
       for (let c = 0; c < this.col; ++c) {
-        for (let i = 0; i < 4; ++i) {
-          context.strokeRect(x + c * FIELD_SIZE, y + (FIELD_SIZE * i), FIELD_SIZE, FIELD_SIZE);
+        let fcp = new FourCharacterPhrasePuzzle.FourCharacterPhrase(FourCharacterPhrasePuzzle.getRandomFCP());
+        for (let i = 0; i < fcp.length; ++i) {
+          context.strokeRect(
+            x + (c * FIELD_SIZE) - FIELD_SIZE_HALF + CHARACTER_SIZE_HALF,
+            y + (FIELD_SIZE * i) - FIELD_SIZE_HALF + CHARACTER_SIZE_HALF + FIELD_SIZE_HALF / 2, FIELD_SIZE, FIELD_SIZE);
+        }
+        for (let i = 0; i < fcp.length; ++i) {
+          context.fillText(fcp.indexOf(i), x + c * FIELD_SIZE, y + (FIELD_SIZE * i) + FIELD_SIZE);
         }
       }
     }
