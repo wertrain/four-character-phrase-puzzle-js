dc9e2504a8861847fc601a39559f67439f3ec21e
wertrain <wertrain@gmail.com>
2020-11-24 23:32:52 +0900

    Add Comment

--- js/util.js
+++ js/util.js
@@ -3,6 +3,10 @@
 
   FourCharacterPhrasePuzzle.Util = {};
 
+  /**
+   * 指定された長さ分の連番の数値が入った配列を取得
+   * @param {*} length 
+   */
   FourCharacterPhrasePuzzle.Util.getArray = (length) => {
     let array = [];
     for (let index = 0; index < length; ++index) {
@@ -10,6 +14,11 @@
     }
     return array;
   };
+
+  /**
+   * 配列をシャッフル
+   * @param {*} array 
+   */
   FourCharacterPhrasePuzzle.Util.shuffleArray = (array) => {
     for(let i = array.length - 1; i > 0; i--){
       let rand = Math.floor(Math.random() * (i + 1));
@@ -18,6 +27,9 @@
     return array;
   };
 
+  /**
+   * 指定された長さ分の連番の数値が入ったシャッフルされた配列を取得
+   */
   FourCharacterPhrasePuzzle.Util.getShuffledArray = (length) => {
     let array = FourCharacterPhrasePuzzle.Util.getArray(length);
     return FourCharacterPhrasePuzzle.Util.shuffleArray(array);
