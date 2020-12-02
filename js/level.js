ae9b96c3d3029af46367fe81268450be7fff8aba
wertrain <wertrain@gmail.com>
2020-12-02 23:28:27 +0900

    WIP

--- js/level.js
+++ js/level.js
@@ -1,12 +1,54 @@
 {
   'use strict';
 
-  FourCharacterPhrasePuzzle.Level = {};
+  const FCPP = FourCharacterPhrasePuzzle;
+
+  FCPP.Level = {};
 
   /**
    * レベル管理を初期化
    */
-  FourCharacterPhrasePuzzle.Level.initialize = () => {
+  FCPP.Level.initialize = () => {
     
   };
+
+  /**
+   * 
+   * @param {*} context 
+   */
+  FCPP.Level.update = (context) => {
+    const canvasWidth = 1280, canvasHeight = 720;
+    const characterField = new FCPP.CharacterField(10, 100, 30);
+    let timer = setInterval(() => {
+      //clearInterval(timer);
+      //context.fillRect(0, 0, canvasWidth, canvasHeight);
+      context.clearRect(0, 0, canvasWidth, canvasHeight);
+      characterField.draw(context);
+    }, 32);
+  };
+
+  /**
+   * 
+   * @param {*} x 
+   * @param {*} y 
+   */
+  FCPP.Level.onMouseDown = (x, y) => {
+    characterField.onMouseDown(x, y)
+  }
+
+  /**
+   * 
+   * @param {*} x 
+   * @param {*} y 
+   */
+  FCPP.Level.onMouseUp = (x, y) => {
+    characterField.onMouseUp(x, y)
+  }
+
+  /**
+   * 
+   */
+  FCPP.Level.onMouseMove = (x, y) => {
+    characterField.onMouseMove(x, y)
+  }
 }
